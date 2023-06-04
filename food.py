a3599537dedaa93994304cbc32b18b8e30a5c434
ncinsli <56553344+ncinsli@users.noreply.github.com>
2023-06-04 22:08:03 +0500

    Добавление еды, установка ленты
    /food/add | remove
    /feed
    /feed/set

--- food.py
+++ food.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, asdict
 import json 
+import os 
 
 @dataclass
 class Dish:
@@ -10,20 +11,46 @@ class Dish:
     mass: int
     avaiable: bool
 
-class Menu:
-    def __init__(self, *dishes):
-        self.dishes = list(dishes)
+class DishCollection:
+    def __init__(self):
+        if not os.path.exists(self.path): open(self.path, 'w').close()
         if self.dishes == []:
-           with open(r'data/menu/avaiable.json', 'r') as f:
-                self.dishes = json.load(f) 
+           with open(self.path, 'r') as f:
+                self.dishes = json.load(f)
 
-    def add(self, dish: Dish):
-        self.dishes.append(dish)
-
-    def remove(self, dish: Dish):
-        self.dishes.remove(dish)
-
-    def serialize(self):
-        with open(r'data/menu/avaiable.json', 'w') as f:
-            json.dump([asdict(dish) for dish in self.dishes], f)
+    def add(self, dish: dict):
+        if dish['name'] not in [d['name'] for d in self.dishes]:
+            self.dishes.append(dish)
+            return True
         
+        return False
+
+    def remove(self, dish: dict):
+        if dish['name'] in [d['name'] for d in self.dishes]:
+            self.dishes.remove(dish)
+            return True
+
+        return False
+    
+    def serialize(self):
+        print(self.dishes)
+        with open(self.path, 'r') as f:
+            prev = f.read()
+
+        with open(self.path, 'w') as f:
+            try:
+                write = json.dumps([dish for dish in self.dishes], indent=2)
+                f.write(write)
+            except: f.write(prev)
+
+class Menu(DishCollection):
+    def __init__(self, *dishes):
+        self.path = r'data/menu/menu.json'
+        self.dishes = list(dishes)
+        super().__init__()
+
+class Feed(DishCollection):
+    def __init__(self, *dishes):
+        self.path = r'data/feed/feed.json'
+        self.dishes = list(dishes)
+        super().__init__()
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import *
 from food import *
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 default_food = [Dish('Хачапури', 120, 'хачапури.jpg', 'Вкусная темка', 700, True), Dish('Лагман', '290', 'лагман.jpg', 'Тоже вкусная темка', 810, False)]
-#menu = Menu(*default_food)
+# menu = Menu(*default_food)
 menu = Menu()
+# menu.serialize()
+feed = Feed()
+# feed.serialize()
 
 @app.route('/food/all', methods=['GET'])
 def all_food():
@@ -14,6 +19,32 @@ def all_food():
 @app.route('/food/avaiable', methods=['GET'])
 def avaiable_food():
     return list(filter(lambda d: d['avaiable'], menu.dishes)), 200
+
+@app.route('/food/add', methods=['POST'])
+def add_dish():
+    res = menu.add(request.get_json())
+    menu.serialize()
+    return ('Ok', 200) if res else ('Dish exists', 409)
+
+@app.route('/food/remove', methods=['POST'])
+def remove_dish():
+    js = request.get_json()
+    print(js)
+    res = menu.remove(js)
+    menu.serialize()
+    return ('Ok', 200) if res else ('Dish doesn\'t exist', 409)
+
+@app.route('/content/feed', methods=["GET"])
+def get_feed():
+    d = feed.dishes[0]
+    print(*feed.dishes)
+    return list(filter(lambda d: d['avaiable'], feed.dishes)), 200
+
+@app.route('/content/feed/set', methods=["POST"])
+def set_feed():
+    feed.dishes = request.get_json()
+    feed.serialize()
+    return 'Ok', 200
 
 if __name__ == '__main__':
     print(menu.dishes)
